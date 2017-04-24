826d28f65a3ddf0994c82c229438f6523e47af22
Pepe Becker <mail@pepebecker.com>
2017-04-24 21:09:51 +0200

    Add mocha tests

--- test/index.js
+++ test/index.js
@@ -1,27 +1,21 @@
 'use strict'
 
 const pinyinOrHanzi = require('../index')
-const so = require('so')
 
-const print = (data) => {
-	switch(data) {
-	case 1:
-		console.log('Mandarin')
-		break
-	case 2:
-		console.log('Pinyin with tone marks')
-		break
-	case 3:
-		console.log('Pinyin with tone numbers')
-		break
-	}
-}
-
-so(function*(){
-	yield pinyinOrHanzi('我的猫喜欢喝牛奶').then(print, console.log)
-	yield pinyinOrHanzi('wǒ de māo xǐhuǎn hé niǔnǎi').then(print, console.log)
-	yield pinyinOrHanzi('wo3 de mao1 xi3huan3 he2 niu3nai3').then(print, console.log)
-	yield pinyinOrHanzi('lǜ').then(print, console.log)
-	yield pinyinOrHanzi('lü4').then(print, console.log)
-	yield pinyinOrHanzi('this is not a chinese language').then(print, console.log)
-})()
+describe('Detect the right language', () => {
+	it('should detect Pinyin with tone numbers', () => {
+		return pinyinOrHanzi('wo3 de mao1 xi3huan3 he2 niu3nai3').then((data) => {
+			data.should.equal(3)
+		})
+	})
+	it('should detect Pinyin with tone marks', () => {
+		return pinyinOrHanzi('wǒ de māo xǐhuǎn hé niǔnǎi').then((data) => {
+			data.should.equal(2)
+		})
+	})
+	it('should detect Mandarin', () => {
+		return pinyinOrHanzi('我的猫喜欢喝牛奶').then((data) => {
+			data.should.equal(1)
+		})
+	})
+})
