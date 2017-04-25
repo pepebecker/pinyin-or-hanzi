173935369dde5bc787df372efd32ab613bc1bb6a
Pepe Becker <mail@pepebecker.com>
2017-04-25 04:11:11 +0200

    Update to take advantage of the multiple characters characters support added to pepebecker/find-hanzi

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const utils = require('pinyin-utils')
 const findHanzi = require('find-hanzi')
-const so = require('so')
 
 const check = (text) => new Promise((yay, nay) => {
 	if (text.match(new RegExp('[a-zA-ZüÜ]+[1-4]'))) {
@@ -19,20 +18,17 @@ const check = (text) => new Promise((yay, nay) => {
 		}
 	}
 	
-	so(function*(){
-		const chars = text.split('')
-		for (let char of chars) {
-			yield findHanzi(char).then((data) => {
-				for (let item of data) {
-					if (text.match(item.hanzi)) {
-						yay(1)
-						return
-					}
+	findHanzi(text, {multiple: true}).then((data) => {
+		if (data && data.length > 0) {
+			for (let item of data) {
+				if (text.match(item.hanzi)) {
+					yay(1)
+					return
 				}
-			}, (error) => nay('pinyin-or-hanzi -> ' + error))
+			}
 		}
 		nay('Not a Chinese language')
-	})()
+	}, (error) => nay('pinyin-or-hanzi -> ' + error))
 })
 
 module.exports = check
