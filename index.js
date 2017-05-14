e82eff8f2a39eb74dfba67c66ff49429e0b972bd
Pepe Becker <mail@pepebecker.com>
2017-05-14 03:28:16 +0200

    Update to use new version of find-hanzi

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const utils = require('pinyin-utils')
 const findHanzi = require('find-hanzi')
+const so = require('so')
 
 const check = (text) => new Promise((yay, nay) => {
 	if (text.match(new RegExp('[a-zA-ZüÜ]+[1-4]'))) {
@@ -18,17 +19,22 @@ const check = (text) => new Promise((yay, nay) => {
 		}
 	}
 	
-	findHanzi(text, {multiple: true}).then((data) => {
-		if (data && data.length > 0) {
-			for (let item of data) {
-				if (item.hanzi && text.match(item.hanzi)) {
-					yay(1)
-					return
-				}
+	so(function*() {
+		let list = []
+		const characters = text.split('')
+		for (let char of characters) {
+			yield findHanzi(char).then((data) => {
+				list = list.concat(data)
+			}, (error) => nay('pinyin-or-hanzi -> ' + error))	
+		}
+		for (let item of list) {
+			if (item.hanzi && text.match(item.hanzi)) {
+				yay(1)
+				return
 			}
 		}
 		yay(0)
-	}, (error) => nay('pinyin-or-hanzi -> ' + error))
+	})()
 })
 
 module.exports = check
